b2ba944013f9fbbf89c50dd90de4c3f87d69914c
Joshua Hull <joshbuddy@gmail.com>
2010-07-22 12:07:28 -0400

    added spec for : in filenames

---
+++ spec/callsite_spec.rb
@@ -0,0 +1,16 @@
+describe 'Callsite' do
+
+  it "should parse dir:name/file:name:12" do
+    @parsed = Callsite.parse('dir:name/file:name:12')
+    @parsed.filename.should == 'dir:name/file:name'
+    @parsed.line.should == 12
+    @parsed.method.should == nil
+  end
+
+  it "should parse dir:name/file:name:12:in `something'" do
+    @parsed = Callsite.parse("dir:name/file:name:12:in `something'")
+    @parsed.filename.should == 'dir:name/file:name'
+    @parsed.line.should == 12
+    @parsed.method.should == 'something'
+  end
+end
